860859385e61bd34b203115ace6087475be532ae
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-12-16 17:17:49 +0700

    Tháp Hà Nội

--- main.cpp
+++ main.cpp
@@ -69,6 +69,34 @@ bool isEven(int n) {
 bool isOdd(int n) {
     return !isEven(n);
 }
+//7.Chuyển cơ số 10 -> 2
+void H10ToH2(int n) {
+    if(n > 0) {
+        int sd = n % 2;
+        n = n / 2;
+        H10ToH2(n);
+        cout<<sd;
+    }
+}
+//8.Tổ hợp chập k của n 
+int choose(int n, int k) {
+    if(k == 0 || n == k) {
+        return 1;
+    }
+    return choose(n - 1, k) + choose(n - 1, k - 1);
+}
+//9.Tháp Hà Nội
+void move(int n, char A, char B, char C) {
+    if(n == 1) 
+        cout<<A<<"=>"<<C<<endl;
+    else
+    {
+        move(n - 1, A, C, B);
+        cout<<A<<"=>"<<C<<endl;
+        move(n - 1, B, A, C);
+    }
+            
+}
 
 int main(int argc, char** argv) { 
     //1st.Linear recursion
@@ -110,16 +138,31 @@ int main(int argc, char** argv) {
     // return 0;
 
     //6th.Mutual Recursion
-    int number = 9;
-    bool result = isEven(number);
-    if (result == true)
-    {
-        cout<<number<<" là số chẵn";
-    } else
-    {
-        cout<<number<<" là số lẻ";
-    }
-    
-    
+    // int number = 9;
+    // bool result = isEven(number);
+    // if (result == true)
+    // {
+    //     cout<<number<<" là số chẵn";
+    // } else
+    // {
+    //     cout<<number<<" là số lẻ";
+    // }        
+    // return 0;
+
+    //7.Chuyển cơ số 10 -> 2
+    // int n = 13;
+    // H10ToH2(n);
+    // return 0;
+
+    //8.Tổ hợp chập k của n 
+    // int n = 5;
+    // int k = 3;
+    // int result = choose(n, k);
+    // cout<<result;
+    // return 0;
+
+    //9.Tháp Hà Nội
+    int n = 3;
+    move(n, 'A','B', 'C');                                        
     return 0;
 }
