7188867691570039a7189c235ce49b7e973e2de9
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-12-20 16:37:18 +0700

    binary search on struct array

--- main.cpp
+++ main.cpp
@@ -22,10 +22,22 @@ using namespace std;
 // void ExportStudentList(Student studentLst[], int size);
 
 //3.Tìm kiếm nhị phân trên mảng cơ sở
-void NhapMang(int M[], int n);
-void XuatMang(int M[], int n);
-int BinarySearch(int M[], int n, int x);
-int BinarySearchRecursion(int M[], int x,  int left, int right);
+// void NhapMang(int M[], int n);
+// void XuatMang(int M[], int n);
+// int BinarySearch(int M[], int n, int x);
+// int BinarySearchRecursion(int M[], int x,  int left, int right);
+
+//4.Tìm kiếm nhị phân mảng cấu trúc
+struct Product
+{
+    int code;
+    char name[150];
+    float price;
+};
+
+void EnterProductList(Product productList[], int n);
+void ExportProductList(Product productList[], int n);
+
 
 int main(int argc, char** argv) { 
        //1.Tìm kiếm tuyến tính mảng cơ sở
@@ -53,23 +65,31 @@ int main(int argc, char** argv) {
         // return 0;
 
     //3.Tìm kiếm nhị phân trên mảng cơ sở   
-    int n = 5;
-    int M[n];
-    NhapMang(M, n);
-    cout<<"Mang sau khi nhap:\n";
-    XuatMang(M, n);
-    cout<<"\nNhap so muon tim:";
-    int x;
-    cin>>x;
-    // int result = BinarySearch(M, n, x);
-    int result = BinarySearchRecursion(M, x, 0, n - 1);
-    if(result == -1) {
-        cout<<"Khong tim thay "<<x;
-    } else        
-        cout<<"Thay "<< x << " tai vi tri " << result;       
+    // int n = 5;
+    // int M[n];
+    // NhapMang(M, n);
+    // cout<<"Mang sau khi nhap:\n";
+    // XuatMang(M, n);
+    // cout<<"\nNhap so muon tim:";
+    // int x;
+    // cin>>x;
+    // // int result = BinarySearch(M, n, x);
+    // int result = BinarySearchRecursion(M, x, 0, n - 1);
+    // if(result == -1) {
+    //     cout<<"Khong tim thay "<<x;
+    // } else        
+    //     cout<<"Thay "<< x << " tai vi tri " << result;       
+    // return 0;
+
+    //4.Tìm kiếm nhị phân mảng cấu trúc
+    int n;
+    cout<<"Nhập số sản phẩm: ";
+    cin>>n;
+    Product productList[n];
+    EnterProductList(productList, n);
+    cout<<"Sản phẩm sau khi nhập:\n";
+    ExportProductList(productList, n);
     return 0;
-
-
 }
 //1.Tìm kiếm tuyến tính mảng cơ sở
 // void NhapMang(int M[], int n) {
@@ -126,53 +146,78 @@ int main(int argc, char** argv) {
 // }
 
 //3.Tìm kiếm nhị phân trên mảng cơ sở
-void NhapMang(int M[], int n) {
-    for (int i = 0; i < n; i++)
-    {
-        cout<<"M["<<i<<"]=";
-        cin>>M[i];
-    }    
-}
+// void NhapMang(int M[], int n) {
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<<"M["<<i<<"]=";
+//         cin>>M[i];
+//     }    
+// }
 
-void XuatMang(int M[], int n) {
+// void XuatMang(int M[], int n) {
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<<M[i]<<"\t";
+//     }
+    
+// }
+
+// int BinarySearch(int M[], int n, int x) {
+//     int left = 0;
+//     int right = n - 1;
+//     int mid;
+//     do
+//     {
+//         mid = (left + right) / 2;
+//         if (M[mid] == x)
+//         {
+//             return mid;
+//         }
+//         else if (M[mid] > x)
+//         {
+//             right = mid - 1;
+//         } else
+//         {
+//             left = mid + 1;
+//         }
+                        
+//     } while (left <= right);     
+
+//     return -1;   
+// }
+
+// int BinarySearchRecursion(int M[], int x, int left, int right) {
+//     if(left > right)
+//         return -1;
+//     int mid = (left + right) / 2; 
+//     if(M[mid] == x)       
+//         return mid;
+//     if (M[mid] > x)
+//         return BinarySearchRecursion(M, x, left, mid - 1);
+//     return BinarySearchRecursion(M, x, mid + 1, right);                        
+// }
+
+//4.Tìm kiếm nhị phân mảng cấu trúc
+void EnterProductList(Product productList[], int n) {
     for (int i = 0; i < n; i++)
     {
-        cout<<M[i]<<"\t";
+        cout<<"Nhập sản phẩm thứ "<<i<<endl;
+        cout<<"Code: ";
+        cin>>productList[i].code;
+        cin.ignore();
+        cout<<"Name: ";
+        fgets(productList[i].name, i, stdin);
+        cin.ignore();
+        cout<<"Price: ";
+        cin>>productList[i].price;
     }
     
 }
-
-int BinarySearch(int M[], int n, int x) {
-    int left = 0;
-    int right = n - 1;
-    int mid;
-    do
+void ExportProductList(Product productList[], int n) {
+    for (int i = 0; i < n; i++)
     {
-        mid = (left + right) / 2;
-        if (M[mid] == x)
-        {
-            return mid;
-        }
-        else if (M[mid] > x)
-        {
-            right = mid - 1;
-        } else
-        {
-            left = mid + 1;
-        }
-                        
-    } while (left <= right);     
-
-    return -1;   
-}
-
-int BinarySearchRecursion(int M[], int x, int left, int right) {
-    if(left > right)
-        return -1;
-    int mid = (left + right) / 2; 
-    if(M[mid] == x)       
-        return mid;
-    if (M[mid] > x)
-        return BinarySearchRecursion(M, x, left, mid - 1);
-    return BinarySearchRecursion(M, x, mid + 1, right);                        
+        Product product = productList[i];
+        cout<<product.code<<"\t"<<product.name<<"\t"<<product.price<<endl;
+    }
+    
 }
