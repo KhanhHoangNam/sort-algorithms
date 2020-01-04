162e206c3948f5148e26a572c6eb85ad4c560295
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2020-01-04 23:01:56 +0700

    buble sort

--- main.cpp
+++ main.cpp
@@ -6,8 +6,53 @@
 
 using namespace std;
 
-
+//1.Bubble sort
+void NhapMang(int M[], int n);
+void XuatMang(int M[], int n);
+void BubbleSort(int M[], int n);
 int main(int argc, char** argv) { 
-       
+    int n;
+    cout<<"Nhap n: ";
+    cin>>n;
+    int M[n];
+    NhapMang(M, n);
+    cout<<"Mang sau khi nhap:\n";
+    XuatMang(M,n);
+    cout<<"\nMang sau khi sap xep:\n";
+    BubbleSort(M, n);
+    XuatMang(M,n);
     return 0;
+}
+void NhapMang(int M[], int n) {
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        M[i] = rand() % 100;
+    }
+    
+}
+void XuatMang(int M[], int n) {
+    for (int i = 0; i < n; i++)
+    {
+        cout<<M[i]<<"\t";
+    }
+    
+}
+
+void BubbleSort(int M[], int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            if (M[j] < M[j - 1])
+            {
+                int temp = M[j];
+                M[j] = M[j - 1];
+                M[j - 1] = temp;
+            }
+            
+        }
+        
+    }
+    
 }
